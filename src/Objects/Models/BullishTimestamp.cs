80dc350e97319b75f31ae654f45ca882126531f3
nils2525 <nilslehb@gmail.com>
2026-02-03 12:55:26 +0100

    added missing file

---
+++ src/Objects/Models/BullishTimestamp.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+using CryptoExchange.Net.Converters.SystemTextJson;
+
+namespace Bullish.Net.Objects.Models
+{
+    public class BullishTimestamp
+    {
+        [JsonPropertyName("timestamp")]
+        [JsonConverter(typeof(DateTimeConverter))]
+        public DateTime Timestamp { get; set; }
+    }
+}
